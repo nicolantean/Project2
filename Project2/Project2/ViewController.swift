b0b74c451eb85d2375d4543bccd705c573689a69
Nicolas Lantean <nlantean@hotmail.com>
2019-04-16 14:59:17 -0300

    Setup

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Nicolas Lantean on 4/16/19.
+//  Copyright © 2019 Nicolas Lantean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
